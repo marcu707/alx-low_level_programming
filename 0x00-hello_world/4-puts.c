fab80182b3212d298a9c256bf77ca293159ddc4f
marcu707 <wilsonfavour80@gmail.com>
2022-09-09 00:30:22 +0100

    My first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #!/bin/bash
-
 #include <stdio.h>
 
 /**
  * main - entry point
- * Description: Print a string using followed by a new line
+ * Description: print a string using followed by a new line
  * Return: 0
-*/
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }

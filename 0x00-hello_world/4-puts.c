6f41efca4909b25791bf3eb0ff8b010dbd84a23b
marcu707 <wilsonfavour80@gmail.com>
2022-09-09 00:15:19 +0100

    my first C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main (void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+}

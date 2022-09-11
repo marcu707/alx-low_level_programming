01564b7ec437dc310aa865dfbcd3fe5d01cd6e8a
marcu707 <wilsonfavour80@gmail.com>
2022-09-11 13:29:11 +0000

    first c compiled

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
  /**
-  * main - Entry point
+  * main - entry point
   *
   *Return: Always 0 (success)
   */
 int main(void)
 {
-	puts("\"programming is like building amultilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

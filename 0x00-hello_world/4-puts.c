5a04c541c480ff9b1d38aa6e0d814702fc19a5d3
marcu707 <wilsonfavour80@gmail.com>
2022-09-11 14:34:51 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
  /**
-  * main-Entry point
-  Description: print a string using followed by a new line
-  *Return: Always 0 (success)
+  * main - Entry point
+  *
+  * Return: Always 0 (success)
   */
 int main(void)
 {

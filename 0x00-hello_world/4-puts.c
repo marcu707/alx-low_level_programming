a6575e572ee3ffa73926d4126395d387e64e71e9
marcu707 <wilsonfavour80@gmail.com>
2022-09-11 15:14:13 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-  /**
-   * main - Entry point
-   *
-   * Return: Always 0 (success)
-   */
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 	char test_str[] = "\"Programming is like building a multilingual puzzle";

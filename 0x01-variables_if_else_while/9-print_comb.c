e86f0088f032f31de95906a58cc11a54b451ea15
marcu707 <wilsonfavour80@gmail.com>
2022-09-12 16:12:05 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int d = 48;
+
+while (d < 58)
+{
+putchar(d);
+if (d != 58)
+{
+putchar(',');
+putchar(' ');
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
